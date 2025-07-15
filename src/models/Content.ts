7ad81cba639a5b7e6cc6ec72505a3137b089d0e7
Aman Kumar <amantop102525@gmail.com>
2025-07-15 21:59:04 +0530

    initlization and mongoose model Schema

---
+++ src/models/Content.ts
@@ -0,0 +1,32 @@
+import mongoose,{Schema,Types} from "mongoose";
+
+const contentTypes = ["image", "video", "article", "audio", "document", "tweet", "youtube", "link"];
+
+const contentSchema=new Schema({
+    link: {
+        type: String,
+        required: true,
+
+    },
+    type:{
+        type: String,
+        enum: contentTypes,
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    tags: [{
+        type: Types.ObjectId,
+        ref: "Tag",
+
+    }],
+    userId: {
+        type: Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+});
+
+export const Content=mongoose.model("Content",contentSchema);
